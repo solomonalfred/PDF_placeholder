901435cb58436ed816a0960124f20b56adca0676
solomonalfred <nik_bogdanov2002@mail.ru>
2023-11-21 17:29:29 +0500

    features

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Запуск api.py
-python3 app/api.py &
+python3 api.py &
 
 # Запуск экземпляров balance_core.py на разных портах
 export PORT=8001
-python3 app/balancer/balance_core.py &
+python3 balancer/balance_core.py &
 
 export PORT=8002
-python3 app/balancer/balance_core.py &
+python3 balancer/balance_core.py &
 
 export PORT=8003
-python3 app/balancer/balance_core.py &
+python3 balancer/balance_core.py &
 
 export PORT=8004
-python3 app/balancer/balance_core.py &
+python3 balancer/balance_core.py &
 
 # Ожидание завершения всех фоновых процессов
