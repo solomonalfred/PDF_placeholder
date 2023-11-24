5780dcc2b81412941434844f8f9fff1e7485d356
solomonalfred <nik_bogdanov2002@mail.ru>
2023-11-25 02:17:00 +0500

    fix balancer

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 cd app
 
 # Запуск скриптов и сохранение их PID
-PORT=8001 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
-PORT=8002 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
+# PORT=8001 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
+# PORT=8002 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
 # PORT=8003 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
 # PORT=8004 python3 balancer/balance_core.py & echo $! >> ../balancer_pids.txt
 
